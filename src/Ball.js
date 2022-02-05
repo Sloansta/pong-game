6496c5636876d9cd2189d83480db754e9e7cd995
Sloan <Sloan.boyce@gmail.com>
2022-02-04 22:51:06 -0500

    added some basic player functions for movement

--- src/Ball.js
+++ src/Ball.js
@@ -1,3 +1,10 @@
 export class Ball {
-    //constructor(x, y)
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.speed = 2;
+    }
+
+    /*getCollision(minX, minY, maxX, maxY) {
+    }*/
 }
--- src/Player.js
+++ src/Player.js
@@ -1,4 +1,22 @@
 // player class will be here.
 export class Player {
-    //constructor(x, )
+    constructor(x) {
+        this.x = x;
+        this.size = 20;
+    }
+
+    getCollision(minX, maxX) {
+        if(this.x < minX) 
+            this.x = 0;
+        else if(this.x > maxX)
+            this.x = maxX;
+    }
+
+    moveRight() {
+        this.x++;
+    }
+
+    moveLeft() {
+        this.x--;
+    }
 }
